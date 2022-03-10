479f4f30285c02513b886260a8e42b5957788582
AkiraJkr <akirajkr1@gmail.com>
2022-03-09 21:48:33 -0300

    added Zanod--Zannor--Zandronum, WHO NAMED THIS
    GZDoom Multiplayer also fixed.

--- ultimate + mouse.bat
+++ ultimate + mouse.bat
@@ -9,36 +9,32 @@ setlocal EnableDelayedExpansion
 set "BASETITLE=Steam Doom Launcher by AkiraJkr and Mad"
 title %BASETITLE%
 
-echo This is made to be used in the "Ultimate Doom" folder, and with a "GZDoom" folder present inside of it.
-echo The Old Launcher requires the IWADs to be present in the "base" folder.
+ECHO Which launcher do you want to use?
+ECHO 1.GZDoom Launcher(Not Ready Yet)
+ECHO 2.Zandronum Launcher(Not Ready Yet)
+ECHO 3.Old GZDoom Launcher
+ECHO 4.Old Zandronum Launcher
 
-rem ECHO Which launcher do you want to use?
-rem ECHO 1.GZDoom Launcher(Not Ready Yet)
-rem ECHO 2.Zanondrum Launcher(Not Ready Yet)
-rem ECHO 3.Old GZDoom Launcher
-rem We don't need this, like why
+CHOICE /C 1234 /M "Enter your choice:"
 
-rem CHOICE /C 123 /M "Enter your choice:"
+IF ERRORLEVEL 4 GOTO oldzandronumlauncher
+IF ERRORLEVEL 3 GOTO oldgzdoomlauncher
+IF ERRORLEVEL 2 GOTO zandronum
+IF ERRORLEVEL 1 GOTO gzdoom
 
-rem IF ERRORLEVEL 3 GOTO oldgzdoomlauncher
-rem IF ERRORLEVEL 2 GOTO zanondrum
-rem IF ERRORLEVEL 1 GOTO gzdoom
+:gzdoom
+START @powershell.exe -ExecutionPolicy Unrestricted -File  "%~dp0\GZDoom_Launcher.ps1"
 
-rem :gzdoom
-rem START @powershell.exe -ExecutionPolicy Unrestricted -File  "%~dp0\GZDoom_Launcher.ps1"
-rem 
-rem :zanondrum
-rem ECHO This is not even done yet, get out
-rem pause
-rem exit
-rem 
-rem :oldgzdoomlauncher
+:Zandronum
+ECHO This is not even done yet, get out
+pause
+exit
+
+:oldgzdoomlauncher
 setx DOOMWADDIR "%~dp0base"
-rem This will set a DOOMWADDIR user environment variable, which will let GZDoom detect DOOM, and whatever WADs/mods you put there.
 cls
 echo Registering IWAD variables for GZDoom on "base" folder. Skip if it's not your first time.
 timeout /t 5
-rem Apparently, a NVMe SSD is not enough. I'm not kidding.
 
 ECHO Do you want to play GZDoom in:
 ECHO 1. Singleplayer
@@ -78,6 +74,11 @@ goto multiplayergamepickerclient
 
 :multiplayerhost
 cls
+
+set /p port="Enter the port to be used or leave blank for default(10666): "
+
+if not defined port set port=10666
+
 set /p playernumbers=Set the number of players that will join your game: 
 
 echo %playernumbers% players will be able to join your game. Wait until everyone joins.
@@ -119,7 +120,7 @@ IF ERRORLEVEL 1 GOTO doommultiplayerclient
 
 :projectbrutalitymultiplayerclient
     echo Initializing GZDoom Client
-    START "" ".\GZDoom\gzdoom.exe" ".\base\Project Brutality 2.03.pk3" -join %ipaddress:"=%:%port%
+    START "" ".\GZDoom\gzdoom.exe" -file .\base\Project_Brutality-master.zip -join %ipaddress:"=%:%port%
 	exit
 
 :d4vmultiplayerclient
@@ -162,32 +163,32 @@ IF ERRORLEVEL 1 GOTO doommultiplayerhost
 
 :doommultiplayerhost
     echo Initializing GZDoom Client and Host
-    START "" ".\GZDoom\gzdoom.exe"  -host %playernumbers%
+    START "" ".\GZDoom\gzdoom.exe"  -host %playernumbers% -port %port%
 	exit
 
 :brutaldoommultiplayerhost
     echo Initializing GZDoom Client and Host
-    START "" ".\GZDoom\gzdoom.exe" ".\base\brutalv21.pk3" -host %playernumbers%
+    START "" ".\GZDoom\gzdoom.exe" ".\base\brutalv21.pk3" -host %playernumbers% -port %port%
 	exit
 
 :projectbrutalitymultiplayerhost
     echo Initializing GZDoom Client and Host
-    START "" ".\GZDoom\gzdoom.exe" ".\base\Project Brutality 2.03.pk3" -host %playernumbers%
+    START "" ".\GZDoom\gzdoom.exe" -file .\base\Project_Brutality-master.zip -host %playernumbers% -port %port%
 	exit
 	
 :d4vmultiplayerhost
     echo Initializing GZDoom Client and Host
-    START "" ".\GZDoom\gzdoom.exe" ".\base\D4V.wad" -host %playernumbers%
+    START "" ".\GZDoom\gzdoom.exe" ".\base\D4V.wad" -host %playernumbers% -port %port%
 	exit
 
 :hellshotsgolfmultiplayerhost
     echo Initializing GZDoom Client and Host
-    START "" ".\GZDoom\gzdoom.exe" ".\base\hellshots_golf-v1.1.pk3" -host %playernumbers%
+    START "" ".\GZDoom\gzdoom.exe" ".\base\hellshots_golf-v1.1.pk3" -host %playernumbers% -port %port%
 	exit
 
 :doom64multiplayerhost
     echo Initializing GZDoom Client and Host
-    START "" ".\GZDoom\gzdoom.exe" ".\base\DOOM64.WAD" -host %playernumbers%
+    START "" ".\GZDoom\gzdoom.exe" ".\base\DOOM64.WAD" -host %playernumbers% -port %port%
 	exit
 	
 :singleplayersetup
@@ -239,3 +240,161 @@ exit
 :End
 ECHO die
 exit
+
+
+
+:oldzandronumlauncher
+setx DOOMWADDIR "%~dp0base"
+cls
+echo Registering IWAD variables for Zandronum on "base" folder. Skip if it's not your first time.
+timeout /t 5
+
+:zanmultiplayersetup
+cls
+
+echo Are you a...
+echo 1. Client
+echo 2. Server Host
+
+CHOICE /C 12 /M "Enter your choice:"
+
+IF ERRORLEVEL 2 GOTO zanmultiplayerhost
+IF ERRORLEVEL 1 GOTO zanmultiplayerclient
+
+:zanmultiplayerclient
+cls
+
+set /p ipaddress="Enter the server's IP Address: "
+
+set /p port="Enter the port to be used or leave blank for default(10666): "
+
+if not defined ipaddress goto zanmultiplayerclient
+
+if not defined port set port=10666
+
+echo You will be joining the server %ipaddress:"=%:%port%
+pause
+goto zanmultiplayergamepickerclient
+
+:zanmultiplayerhost
+cls
+
+set /p port="Enter the port to be used or leave blank for default(10666): "
+
+if not defined port set port=10666
+
+set /p playernumbers=Set the number of players that will join your game: 
+
+echo %playernumbers% players will be able to join your game. Wait until everyone joins.
+
+goto zanmultiplayergamepickerhost
+
+:zanmultiplayergamepickerclient
+
+ECHO Which version of DOOM do you wish to start? [This] means it requires a certain IWAD present.
+ECHO 1.Vanilla DOOM
+ECHO 2.Brutal DOOM v21
+ECHO 3.Project Brutality v2
+ECHO 4.Doom 4 Vanilla
+ECHO 5.Hellshots Golf[Doom2]
+ECHO 6.Doom 64
+ECHO Or else...
+ECHO 7 to cancel the Launcher. You could just, like press that X on this console window. Please. Stop. Get some help.
+
+CHOICE /C 1234567 /M "Enter your choice:"
+
+:: Note - list ERRORLEVELS in decreasing order
+IF ERRORLEVEL 7 GOTO End
+IF ERRORLEVEL 6 GOTO zandoom64multiplayerclient
+IF ERRORLEVEL 5 GOTO zanhellshotsgolfmultiplayerclient
+IF ERRORLEVEL 4 GOTO zand4vmultiplayerclient
+IF ERRORLEVEL 3 GOTO zanprojectbrutalitymultiplayerclient
+IF ERRORLEVEL 2 GOTO zanbrutaldoommultiplayerclient
+IF ERRORLEVEL 1 GOTO zandoommultiplayerclient
+
+:zandoommultiplayerclient
+    echo Initializing Zandronum Client
+    START "" ".\Zandronum\zandronum.exe" -connect %ipaddress:"=%:%port% -width 1280 -height 720
+	exit
+
+:zanbrutaldoommultiplayerclient
+    echo Initializing Zandronum Client
+    START "" ".\Zandronum\zandronum.exe" ".\base\brutalv21.11.3.pk3" -connect %ipaddress:"=%:%port% -width 1280 -height 720
+	exit
+
+:zanprojectbrutalitymultiplayerclient
+    echo Initializing Zandronum Client
+    START "" ".\Zandronum\zandronum.exe" -file .\base\PB3Zv1.0.2-FINAL.pk3 -connect %ipaddress:"=%:%port% -width 1280 -height 720
+	exit
+
+:zand4vmultiplayerclient
+    echo Initializing Zandronum Client
+    START "" ".\Zandronum\zandronum.exe" ".\base\D4V.wad" -connect %ipaddress:"=%:%port% -width 1280 -height 720
+	exit
+
+:zanhellshotsgolfmultiplayerclient
+    echo Initializing Zandronum Client
+    START "" ".\Zandronum\zandronum.exe" ".\base\hellshots_golf-v1.1.pk3" -connect %ipaddress:"=%:%port% -width 1280 -height 720
+	exit
+
+:zandoom64multiplayerclient
+    echo Initializing Zandronum Client
+    START "" ".\Zandronum\zandronum.exe" ".\base\DOOM64.WAD" -connect %ipaddress:"=%:%port% -width 1280 -height 720
+	exit
+
+:zanmultiplayergamepickerhost
+
+ECHO Which version of DOOM do you wish to start? [This] means it requires a certain IWAD present.
+ECHO 1.Vanilla DOOM
+ECHO 2.Brutal DOOM v21
+ECHO 3.Project Brutality v2
+ECHO 4.Doom 4 Vanilla
+ECHO 5.Hellshots Golf[Doom2]
+ECHO 6.Doom 64
+ECHO Or else...
+ECHO 7 to cancel the Launcher. You could just, like press that X on this console window. Please. Stop. Get some help.
+
+CHOICE /C 1234567 /M "Enter your choice:"
+
+:: Note - list ERRORLEVELS in decreasing order
+IF ERRORLEVEL 7 GOTO End
+IF ERRORLEVEL 6 GOTO zandoom64multiplayerhost
+IF ERRORLEVEL 5 GOTO zanhellshotsgolfmultiplayerhost
+IF ERRORLEVEL 4 GOTO zand4vmultiplayerhost
+IF ERRORLEVEL 3 GOTO zanprojectbrutalitymultiplayerhost
+IF ERRORLEVEL 2 GOTO zanbrutaldoommultiplayerhost
+IF ERRORLEVEL 1 GOTO zandoommultiplayerhost
+
+:zandoommultiplayerhost
+    echo Initializing Zandronum Client and Host
+    START "" ".\Zandronum\zandronum.exe"  -host %playernumbers% -port %port% -private -upnp -width 1280 -height 720
+	exit
+
+:zanbrutaldoommultiplayerhost
+    echo Initializing Zandronum Client and Host
+    START "" ".\Zandronum\zandronum.exe" ".\base\brutalv21.11.3.pk3" -host %playernumbers% -port %port% -private -upnp -width 1280 -height 720
+	exit
+
+:zanprojectbrutalitymultiplayerhost
+    echo Initializing Zandronum Client and Host
+    START "" ".\Zandronum\zandronum.exe" -file .\base\PB3Zv1.0.2-FINAL.pk3 -host %playernumbers% -port %port% -private -upnp -width 1280 -height 720
+	exit
+	
+:zand4vmultiplayerhost
+    echo Initializing Zandronum Client and Host
+    START "" ".\Zandronum\zandronum.exe" ".\base\D4V.wad" -host %playernumbers% -port %port% -private -upnp -width 1280 -height 720
+	exit
+
+:zanhellshotsgolfmultiplayerhost
+    echo Initializing Zandronum Client and Host
+    START "" ".\Zandronum\zandronum.exe" ".\base\hellshots_golf-v1.1.pk3" -host %playernumbers% -port %port% -private -upnp -width 1280 -height 720
+	exit
+
+:zandoom64multiplayerhost
+    echo Initializing Zandronum Client and Host
+    START "" ".\Zandronum\zandronum.exe" ".\base\DOOM64.WAD" -host %playernumbers% -port %port% -private -upnp -width 1280 -height 720
+	exit
+
+:End
+	ECHO die
+	exit
